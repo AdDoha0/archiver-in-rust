a38da9c0b873cad8ace35ddad9bf5d9bc1dda2aa
Adam <adamdoxaev5@gmail.com>
2025-03-01 17:11:46 +0300

    getting arguments from the command line

--- src/commands.rs
+++ src/commands.rs
@@ -1,1 +1,55 @@
-use crate::archive;
+
+use clap::{Parser, Subcommand,};
+
+
+/// Основная структура команд CLI
+#[derive(Parser)]
+#[command(name = "myarchiver", version = "1.0", about = "Простой CLI-архиватор")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+
+/// Перечисление доступных команд
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Упаковать файлы в архив
+    Pack {
+        /// Путь к файлам, которые нужно упаковать
+        #[arg(required = true)]
+        input: Vec<String>,
+
+        /// Имя выходного архива
+        #[arg(short, long, default_value = "archive.zip")]
+        output: String,
+    },
+
+    /// Распаковать архив
+    Unpack {
+        /// Имя архива для распаковки
+        #[arg(required = true)]
+        archive: String,
+
+        /// Папка, куда извлекать файлы
+        #[arg(short, long, default_value = "./extracted")]
+        output_dir: String,
+    },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-mod commands;
+use clap::{Parser};
 
+mod commands;
+mod archive;
+
+use commands::Commands::{Pack, Unpack};
+use commands::Cli;
 
 
 fn main() {
-    println!("Hello, world!");
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Pack { input, output } => {
+            println!("Упаковка файлов {:?} в {}", input, output);
+        }
+        Unpack { archive, output_dir } => {
+            println!("Распаковка {} в {}", archive, output_dir);
+            // Тут вызываем функцию разархивирования
+        }
+    }
+
+
+
 }
